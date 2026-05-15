9f0fe5435d3cd02879e44c400b88fd0f83cf87ce
ryankristianruiz <ryankristian.ruiz@wvsu.edu.ph>
2026-05-15 11:31:36 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'abotkamay-v16';   // change to v2, v3 etc. when you push a new release
+const CACHE_NAME = 'abotkamay-v17';   // change to v2, v3 etc. when you push a new release
 const urlsToCache = [
   './',
   './index.html',
